e6cbb6a476a1974ac1dd732bc031fbb97dd7d969
KIM JIHYUN <rlawlgus1323@naver.com>
2025-07-30 23:36:27 +0900

    feat: 프로그래머스 문제 풀이

---
+++ AlgorithmPractice/src/programmers/rotateStringPrinter/Main.java
@@ -0,0 +1,16 @@
+package programmers.rotateStringPrinter;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        
+        for (int i = 0; i < a.length(); i++) {
+        	System.out.println(a.toCharArray()[i]);
+        }
+       
+    }
+
+}
