b00d0c25a15f3eb84c6cbe6057d530b59b12cbfe
KIM JIHYUN <rlawlgus1323@naver.com>
2025-08-04 23:48:00 +0900

    feat : 2025/08/04 문제풀이

---
+++ AlgorithmPractice/src/programmers/divisibility/Main.java
@@ -0,0 +1,12 @@
+package programmers.divisibility;
+
+public class Main {
+	public static void main(String[] args) {
+        System.out.println(solution(98, 2));   // 출력: 1
+        System.out.println(solution(34, 3));   // 출력: 0
+    }
+
+    public static int solution(int num, int n) {
+        return (num % n == 0) ? 1 : 0;
+    }
+}
