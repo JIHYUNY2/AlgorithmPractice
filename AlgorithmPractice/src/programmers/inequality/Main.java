1b903b80635c56bc9059caa46317a83ba383e827
KIM JIHYUN <85729769+JIHYUNY2@users.noreply.github.com>
2025-08-11 01:06:17 +0900

    feat : 2025/08/10 프로그래머스 문제풀이

---
+++ AlgorithmPractice/src/programmers/inequality/Main.java
@@ -0,0 +1,19 @@
+package programmers.inequality;
+// ineq은 </> , eq는 =/! 중 하나임. n과 m이 주어질 때 조건에 맞으면 1, 아니면 0 리턴
+public class Main {
+	public static void main(String args[]) {
+		System.out.println(solution(">", "=", 5, 3));
+		System.out.println(solution("<", "!", 5, 5));  // 0 (5 < 5는 거짓)
+        System.out.println(solution("<", "=", 3, 3));  // 1 (3 <= 3)
+        System.out.println(solution(">", "!", 2, 7));  // 0 (2 > 7 거짓)
+	}
+	public static int solution(String ineq, String eq, int n, int m) {
+		boolean result;
+		if(ineq.equals("<")) {
+			result = eq.equals("=") ? n <= m : n < m;
+		} else {
+			result = eq.equals("=") ? n >= m : n > m;
+		}
+		return result ? 1 : 0 ;
+	}
+}
